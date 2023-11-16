071e496af52a1e8125a6a056f453503b439f5763
Akrem Boughazi <kikoua203@gmail.com>
2023-11-16 14:47:36 +0100

    updating in the completion parameter

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -6,21 +6,19 @@ const openai = require('../utils/openaiConfig')
 //@route POST /api/v1/openai/generate
 //@access privet
 const generateDescription = asyncHandler(async (req, res, next)=>{
-    const {topic} = req.body
-    if (topic ?? true){
-        return next(new ErrHandeling("please provide a topic", 400))
-    }
+    const {Role, Number_of_Posts_to_Generate, Social_Media_Platform, Tone, Avoid, Emoji, Number_of_Words} = req.body
+
     const COMPLETION = await openai.chat.completions.create({
         message:[{
-            role: "user", content: `Come up with a description for YouTube video called ${topic}.`
+            role: "user", 
+            content: `Act as a ${Role} to generate ${Number_of_Posts_to_Generate} posts on ${Social_Media_Platform}. Inspire {Audience} to embrace $[Topic}. Maintain a ${Tone} tone, avoid ${Avoid}, ${Emoji && 'and use emojis to make it engaging'}. Each post should be concise, around ${Number_of_Words}`
         }],
         model: "gpt-3.5-turbo",
         max_tokens: 100
     })
-    console.log("done")
+    
     const result = COMPLETION.choices[0].message.content.replace(/\n/g, '')
-    console.log(result)
-    res.status(200).json({message: result})
+    return res.status(200).json({message: result})
 })
 
 module.exports = {
