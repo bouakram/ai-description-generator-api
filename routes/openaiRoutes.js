14830ad0d2158bc65f870f4aad5f35e9643f5909
Akrem Boughazi <kikoua203@gmail.com>
2023-11-17 16:16:56 +0100

    deleted files and adding some new dep and changing in prisma schema

--- routes/openaiRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { generateDescription } = require('../controllers/openaiController');
-const Router = express.Router()
-
-Router.route('/generate').post(generateDescription)
-
-module.exports = Router
--- utils/openaiConfig.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config()
-
-const OpenAI = require('openai')
-const openai = new OpenAI({ apiKey: process.env.API_KEY });
-
-module.exports = openai
