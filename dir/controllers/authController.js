1423458ee3ae8d6ea8a4eb3097c21473b2707cb9
Akrem Boughazi <kikoua203@gmail.com>
2023-11-19 21:45:34 +0100

    adding google-auth register login logout

--- dir/controllers/authController.js
+++ dir/controllers/authController.js
@@ -1,21 +1,95 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const asyncHandler = require('express-async-handler');
-const ErrHandeling = require('../utils/ErrorHandler');
-const prisma = require('../utils/prismaClient');
+exports.logOut = exports.logIn = exports.register = exports.googleAuthCallback = void 0;
+const express_async_handler_1 = __importDefault(require("express-async-handler"));
+const ErrorHandler_1 = __importDefault(require("../utils/ErrorHandler"));
+const prismaClient_1 = require("../utils/prismaClient");
+const bcrypt_1 = __importDefault(require("bcrypt"));
+const dotenv_1 = require("dotenv");
+const generateToken_1 = __importDefault(require("../token/generateToken"));
+(0, dotenv_1.config)();
+const googleAuthCallback = (req, res) => {
+    // Successful authentication, redirect home.
+    if (req.user) {
+        const AUTHUSER = req.user;
+        res.cookie('token', (0, generateToken_1.default)(AUTHUSER.id), {
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== 'development',
+            sameSite: 'strict',
+            maxAge: 15 * 24 * 60 * 60 * 1000
+        });
+    }
+    res.redirect(process.env.FRONT_URL);
+};
+exports.googleAuthCallback = googleAuthCallback;
+//@desc register user
+//@route /api/v1/auth/register
+//@access public
+exports.register = (0, express_async_handler_1.default)(async (req, res, next) => {
+    const { email, username, password } = req.body;
+    if (!email || !username || !password) {
+        return next(new ErrorHandler_1.default("enter all feald please", 400));
+    }
+    const USER = await prismaClient_1.prisma.user.findUnique({
+        where: {
+            email
+        }
+    });
+    if (USER) {
+        return next(new ErrorHandler_1.default("not allowed to register with this email, try another email please", 400));
+    }
+    const salt = await bcrypt_1.default.genSalt(10);
+    const hashedPassword = await bcrypt_1.default.hash(password, salt);
+    const CREATED_USER = await prismaClient_1.prisma.user.create({
+        data: {
+            email,
+            username,
+            password: hashedPassword,
+        }
+    });
+    res.cookie('token', (0, generateToken_1.default)(CREATED_USER.id), {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== 'development',
+        sameSite: 'strict',
+        maxAge: 15 * 24 * 60 * 60 * 1000
+    });
+    res.status(200).json({ message: 'User regestred successfully' });
+});
 //@desc login user
 //@route /api/v1/auth/login
 //@access public
-asyncHandler(async function logIn(req, res, next) {
+exports.logIn = (0, express_async_handler_1.default)(async (req, res, next) => {
+    const { email, password } = req.body;
+    const USER = await prismaClient_1.prisma.user.findUnique({
+        where: {
+            email
+        }
+    });
+    if (USER && bcrypt_1.default.compare(USER.password, password)) {
+        res.cookie('token', (0, generateToken_1.default)(USER.id), {
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== 'development',
+            sameSite: 'strict',
+            maxAge: 15 * 24 * 60 * 60 * 1000
+        });
+        res.status(200).json({ message: "user logd in successfully" });
+    }
 });
-//@desc login user
-//@route /api/v1/auth/signin
-//@access public
-asyncHandler(async function signIn(req, res, next) {
-});
-//@desc login user
+//@desc logout user
 //@route /api/v1/auth/logout
 //@access public
-asyncHandler(async function logOut(req, res, next) {
+exports.logOut = (0, express_async_handler_1.default)(async (req, res, next) => {
+    const coockie = req.cookies;
+    if (!coockie.token) {
+        return next(new ErrorHandler_1.default("nomthing went wrong try again!", 400));
+    }
+    res.clearCookie('token', {
+        httpOnly: true,
+        secure: true
+    });
+    res.status(203).redirect(process.env.FRONT_URL);
 });
 //# sourceMappingURL=authController.js.map
--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,24 +1,94 @@
 import type { Response, Request, NextFunction } from "express"
+import asyncHandler from 'express-async-handler'
+import ErrHandeling from '../utils/ErrorHandler'
+import { prisma } from '../utils/prismaClient'
+import bcrypt from 'bcrypt'
+import { config } from 'dotenv'
+import generateToken from "../token/generateToken"
+import { User } from "@prisma/client"
+config()
 
-const asyncHandler = require('express-async-handler')
-const ErrHandeling = require('../utils/ErrorHandler')
-const prisma = require('../utils/prismaClient')
+export const googleAuthCallback = (req: Request, res: Response) => {
+    // Successful authentication, redirect home.
+    if (req.user) {
+        const AUTHUSER = req.user as User
+        res.cookie('token', generateToken(AUTHUSER.id), {
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== 'development',
+            sameSite: 'strict',
+            maxAge: 15 * 24 * 60 * 60 * 1000
+        })
+    }
+    res.redirect(process.env.FRONT_URL);
+}
+
+//@desc register user
+//@route /api/v1/auth/register
+//@access public
+export const register = asyncHandler(async (req, res, next) => {
+    const { email, username, password }: User = req.body
+    if (!email || !username || !password) {
+        return next(new ErrHandeling("enter all feald please", 400))
+    }
+    const USER = await prisma.user.findUnique({
+        where: {
+            email
+        }
+    })
+    if (USER) {
+        return next(new ErrHandeling("not allowed to register with this email, try another email please", 400))
+    }
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+    const CREATED_USER = await prisma.user.create({
+        data: {
+            email,
+            username,
+            password: hashedPassword,
+        } as User
+    })
+    res.cookie('token', generateToken(CREATED_USER.id), {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== 'development',
+        sameSite: 'strict',
+        maxAge: 15 * 24 * 60 * 60 * 1000
+    })
+    res.status(200).json({ message: 'User regestred successfully' })
+})
 
 //@desc login user
 //@route /api/v1/auth/login
 //@access public
-asyncHandler(async function logIn(req: Response, res: Response, next: NextFunction) {
+export const logIn = asyncHandler(async (req, res, next) => {
+    const { email, password }: User = req.body
+    const USER = await prisma.user.findUnique({
+        where: {
+            email
+        } as User
+    })
 
+    if (USER && bcrypt.compare(USER.password, password)) {
+        res.cookie('token', generateToken(USER.id), {
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== 'development',
+            sameSite: 'strict',
+            maxAge: 15 * 24 * 60 * 60 * 1000
+        })
+        res.status(200).json({ message: "user logd in successfully" })
+    }
 })
-//@desc login user
-//@route /api/v1/auth/signin
-//@access public
-asyncHandler(async function signIn(req: Request, res: Response, next: NextFunction) {
 
-})
-//@desc login user
+//@desc logout user
 //@route /api/v1/auth/logout
 //@access public
-asyncHandler(async function logOut(req: Request, res: Response, next: NextFunction) {
-
+export const logOut = asyncHandler(async (req, res, next) => {
+    const coockie = req.cookies
+    if (!coockie.token) {
+        return next(new ErrHandeling("nomthing went wrong try again!", 400))
+    }
+    res.clearCookie('token', {
+        httpOnly: true,
+        secure: true
+    })
+    res.status(203).redirect(process.env.FRONT_URL)
 })
