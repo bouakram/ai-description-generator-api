fbe92db88406523d6e70b06a2c165369676be964
Akrem Boughazi <kikoua203@gmail.com>
2023-12-02 01:10:27 +0100

    adding openai type in ts

--- src/utils/openaiConfig.ts
+++ src/utils/openaiConfig.ts
@@ -2,6 +2,6 @@ import OpenAI from 'openai';
 import { config } from 'dotenv'
 config()
 
-const openai = new OpenAI({ apiKey: process.env.API_KEY });
+const openai: OpenAI = new OpenAI({ apiKey: process.env.API_KEY });
 
 export default openai
