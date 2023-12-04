fad93af1ffc9e9ede29c43ba6bc61ab24fb3f7f7
Akrem Boughazi <kikoua203@gmail.com>
2023-12-04 02:08:59 +0100

    adding some new todos tasks

--- dir/routes/openaiRoutes.js
+++ dir/routes/openaiRoutes.js
@@ -4,5 +4,7 @@ const express = require('express');
 const { generateDescription } = require('../controllers/openaiController');
 const Router = express.Router();
 Router.route('/generate').post(generateDescription);
+// TODO: adding new routes for save and remove the generated content
+// Router.route('/generate/content').post(saveGeneratedContent).delete(deleteGeneratedContent)
 exports.default = Router;
 //# sourceMappingURL=openaiRoutes.js.map
--- src/routes/openaiRoutes.ts
+++ src/routes/openaiRoutes.ts
@@ -3,5 +3,7 @@ const { generateDescription } = require('../controllers/openaiController');
 const Router = express.Router()
 
 Router.route('/generate').post(generateDescription)
+// TODO: adding new routes for save and remove the generated content
+// Router.route('/generate/content').post(saveGeneratedContent).delete(deleteGeneratedContent)
 
 export default Router
