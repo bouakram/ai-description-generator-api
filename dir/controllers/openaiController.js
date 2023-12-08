f59fe1094eabe439ed1a3cd029643852e3f84845
Akrem Boughazi <kikoua203@gmail.com>
2023-12-09 00:55:22 +0100

    adding new function in the controller for geting content deleting and saving

--- dir/controllers/openaiController.js
+++ dir/controllers/openaiController.js
@@ -3,9 +3,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateDescription = void 0;
+exports.deleteContent = exports.getLastContent = exports.getContent = exports.getTopic = exports.saveGeneratedContent = exports.generateDescription = void 0;
 const express_async_handler_1 = __importDefault(require("express-async-handler"));
+const ErrorHandler_1 = __importDefault(require("../utils/ErrorHandler"));
 const openaiConfig_1 = __importDefault(require("../utils/openaiConfig"));
+const prismaClient_1 = require("../utils/prismaClient");
 // TODO: change the name of the controller ind give it a unique name match the content of the controller
 // TODO: generateDescription controller need update
 // TODO: generateDescription controller need optimizations
@@ -38,8 +40,117 @@ exports.generateDescription = (0, express_async_handler_1.default)(async (req, r
         const airesult = Completion.choices[0].message.content.replace(/\n/g, '');
         result.push(airesult);
     }
-    console.log("result -----------------");
-    console.log(result);
     res.status(200).json({ message: result });
+});
+// TODO: adding zod validation
+exports.saveGeneratedContent = (0, express_async_handler_1.default)(async (req, res, next) => {
+    const user = req.user;
+    const { topic, contentGenerated, platform } = req.body;
+    const createdTopic = await prismaClient_1.prisma.topic.create({
+        data: {
+            topic: topic
+        }
+    });
+    if (!createdTopic)
+        return next(new ErrorHandler_1.default("somthing went wrong", 404));
+    contentGenerated.forEach(async (cont) => {
+        await prismaClient_1.prisma.content.create({
+            data: {
+                userId: user.id,
+                topicId: createdTopic.id,
+                plateformname: platform,
+                body: cont
+            }
+        });
+    });
+    res.status(201).json({ message: "successfully saved" });
+});
+//@desc get topic
+//@route GET /api/v1/openai/topic
+//@access private
+exports.getTopic = (0, express_async_handler_1.default)(async (req, res, next) => {
+    const topic = await prismaClient_1.prisma.topic.findMany({
+        take: 10
+    });
+    if (!topic)
+        return next(new ErrorHandler_1.default("no topic found", 404));
+    res.status(200).json({ topic: topic });
+});
+//@desc get user content
+//@route GET /api/v1/openai/content
+//@access private
+exports.getContent = (0, express_async_handler_1.default)(async (req, res, next) => {
+    const user = req.user;
+    const platform = req.query.platform;
+    let query = {};
+    if (platform) {
+        query = { userId: user.id, plateformname: platform };
+    }
+    else {
+        query = { userId: user.id };
+    }
+    const content = await prismaClient_1.prisma.content.findMany({
+        where: query,
+        select: {
+            id: true,
+            plateformname: true,
+            topic: {
+                select: {
+                    topic: true
+                }
+            },
+            body: true
+        }
+    });
+    if (!content)
+        return next(new ErrorHandler_1.default("no content found", 404));
+    res.status(200).json({ content: content });
+});
+//@desc get user last content
+//@route GET /api/v1/openai/last-content
+//@access private
+exports.getLastContent = (0, express_async_handler_1.default)(async (req, res, next) => {
+    const user = req.user;
+    const content = await prismaClient_1.prisma.content.findMany({
+        where: {
+            userId: user.id,
+        },
+        select: {
+            plateformname: true,
+            topic: true
+        },
+        take: 5
+    });
+    if (!content)
+        return next(new ErrorHandler_1.default("no content found", 404));
+    res.status(200).json({ content: content });
+});
+//@desc delete user content
+//@route Delete /api/v1/openai/content/:id
+//@access private
+exports.deleteContent = (0, express_async_handler_1.default)(async (req, res, next) => {
+    const id = req.params.id;
+    const content = await prismaClient_1.prisma.content.delete({
+        where: {
+            id: id,
+        },
+    });
+    const stillContent = await prismaClient_1.prisma.content.findFirst({
+        where: {
+            topicId: content.topicId
+        }
+    });
+    if (!stillContent) {
+        const topic = await prismaClient_1.prisma.topic.delete({
+            where: {
+                id: content.topicId
+            }
+        });
+        if (!topic)
+            return next(new ErrorHandler_1.default("no topic found", 404));
+    }
+    if (!content)
+        return next(new ErrorHandler_1.default("no content found", 404));
+    res.status(200).json({ message: "successfully deleted" });
 });
 //# sourceMappingURL=openaiController.js.map
--- src/controllers/openaiController.ts
+++ src/controllers/openaiController.ts
@@ -1,10 +1,11 @@
-import type { NextFunction, Request, Response } from "express"
-import { PromtTypes } from "types"
+import { ContentGenerated, PromtTypes } from "types"
 
 import asyncHandler from 'express-async-handler'
 import ErrHandeling from '../utils/ErrorHandler'
 import openai from '../utils/openaiConfig'
 import type { ChatCompletion } from "openai/resources/index.mjs"
+import { prisma } from "../utils/prismaClient"
+import { User } from "@prisma/client"
 
 // TODO: change the name of the controller ind give it a unique name match the content of the controller
 // TODO: generateDescription controller need update
@@ -39,8 +40,121 @@ export const generateDescription = asyncHandler(async (req, res, next) => {
         const airesult = Completion.choices[0].message.content.replace(/\n/g, '')
         result.push(airesult)
     }
-    console.log("result -----------------")
-    console.log(result)
 
     res.status(200).json({ message: result })
+})
+
+// TODO: adding zod validation
+
+export const saveGeneratedContent = asyncHandler(async (req, res, next) => {
+    const user = req.user as Pick<User, "id" | "email">;
+    const { topic, contentGenerated, platform }: ContentGenerated = req.body
+
+    const createdTopic = await prisma.topic.create({
+        data: {
+            topic: topic
+        }
+    })
+    if (!createdTopic) return next(new ErrHandeling("somthing went wrong", 404))
+
+    contentGenerated.forEach(async (cont) => {
+        await prisma.content.create({
+            data: {
+                userId: user.id,
+                topicId: createdTopic.id,
+                plateformname: platform,
+                body: cont
+            }
+        })
+    })
+
+    res.status(201).json({ message: "successfully saved" })
+})
+
+//@desc get topic
+//@route GET /api/v1/openai/topic
+//@access private
+export const getTopic = asyncHandler(async (req, res, next) => {
+    const topic = await prisma.topic.findMany({
+        take: 10
+    })
+    if (!topic) return next(new ErrHandeling("no topic found", 404))
+    res.status(200).json({ topic: topic })
+})
+
+//@desc get user content
+//@route GET /api/v1/openai/content
+//@access private
+export const getContent = asyncHandler(async (req, res, next) => {
+    const user = req.user as Pick<User, "id" | "email">;
+    const platform = req.query.platform
+    let query: object = {}
+    if (platform) {
+        query = { userId: user.id, plateformname: platform }
+    } else {
+        query = { userId: user.id }
+    }
+    const content = await prisma.content.findMany({
+        where: query,
+        select: {
+            id: true,
+            plateformname: true,
+            topic: {
+                select: {
+                    topic: true
+                }
+            },
+            body: true
+        }
+    })
+    if (!content) return next(new ErrHandeling("no content found", 404))
+    res.status(200).json({ content: content })
+})
+
+//@desc get user last content
+//@route GET /api/v1/openai/last-content
+//@access private
+export const getLastContent = asyncHandler(async (req, res, next) => {
+    const user = req.user as Pick<User, "id" | "email">;
+    const content = await prisma.content.findMany({
+        where: {
+            userId: user.id,
+        },
+        select: {
+            plateformname: true,
+            topic: true
+        },
+        take: 5
+    });
+
+    if (!content) return next(new ErrHandeling("no content found", 404))
+    res.status(200).json({ content: content })
+})
+
+//@desc delete user content
+//@route Delete /api/v1/openai/content/:id
+//@access private
+export const deleteContent = asyncHandler(async (req, res, next) => {
+    const id = req.params.id
+    const content = await prisma.content.delete({
+        where: {
+            id: id,
+        },
+    });
+    const stillContent = await prisma.content.findFirst({
+        where: {
+            topicId: content.topicId
+        }
+    })
+    if (!stillContent) {
+        const topic = await prisma.topic.delete({
+            where: {
+                id: content.topicId
+            }
+        })
+        if (!topic) return next(new ErrHandeling("no topic found", 404))
+    }
+
+    if (!content) return next(new ErrHandeling("no content found", 404))
+    res.status(200).json({ message: "successfully deleted" })
 })
