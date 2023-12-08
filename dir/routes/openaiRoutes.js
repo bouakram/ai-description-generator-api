07a4d9fd9bddb1c9697ec38877f915c953526707
Akrem Boughazi <kikoua203@gmail.com>
2023-12-09 00:50:46 +0100

    adding new routes for saving geting content and deleteing content

--- dir/routes/openaiRoutes.js
+++ dir/routes/openaiRoutes.js
@@ -1,10 +1,19 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const openaiController_1 = require("../controllers/openaiController");
+const authMiddleware_1 = __importDefault(require("../middleware/authMiddleware"));
 const express = require('express');
 const { generateDescription } = require('../controllers/openaiController');
 const Router = express.Router();
-Router.route('/generate').post(generateDescription);
-// TODO: adding new routes for save and remove the generated content
-// Router.route('/generate/content').post(saveGeneratedContent).delete(deleteGeneratedContent)
+Router.route('/topic').get(authMiddleware_1.default, openaiController_1.getTopic);
+Router.route('/content').get(authMiddleware_1.default, openaiController_1.getContent);
+Router.route('/last-content').get(authMiddleware_1.default, openaiController_1.getLastContent);
+Router.route('/generate').post(authMiddleware_1.default, generateDescription);
+///// TODO: adding new routes for save and remove the generated content
+Router.route('/content').post(authMiddleware_1.default, openaiController_1.saveGeneratedContent);
+Router.route('/content/:id').delete(authMiddleware_1.default, openaiController_1.deleteContent);
 exports.default = Router;
 //# sourceMappingURL=openaiRoutes.js.map
--- src/routes/openaiRoutes.ts
+++ src/routes/openaiRoutes.ts
@@ -1,9 +1,16 @@
+import { deleteContent, getContent, getLastContent, getTopic, saveGeneratedContent } from "../controllers/openaiController";
+import protectUser from "../middleware/authMiddleware";
+
 const express = require('express');
 const { generateDescription } = require('../controllers/openaiController');
 const Router = express.Router()
 
-Router.route('/generate').post(generateDescription)
-// TODO: adding new routes for save and remove the generated content
-// Router.route('/generate/content').post(saveGeneratedContent).delete(deleteGeneratedContent)
+Router.route('/topic').get(protectUser, getTopic)
+Router.route('/content').get(protectUser, getContent)
+Router.route('/last-content').get(protectUser, getLastContent)
+Router.route('/generate').post(protectUser, generateDescription)
+///// TODO: adding new routes for save and remove the generated content
+Router.route('/content').post(protectUser, saveGeneratedContent)
+Router.route('/content/:id').delete(protectUser, deleteContent)
 
 export default Router
